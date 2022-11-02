1da28379b6fcd98f87a71e0dc25f607d9d65fd20
Mrmcghie <53179879+Mrmcghie@users.noreply.github.com>
2022-11-01 21:09:18 -0400

    Update main.tf

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -1,5 +1,5 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "aws_access_key" {AKIAW34TF7FCFRJSVB6C}
+variable "aws_secret_key" {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}
 
 provider "aws" {
   access_key = var.aws_access_key
