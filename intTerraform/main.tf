e1e9b982f7d2c514f7b8963c24de77845e0c33a9
Mrmcghie <53179879+Mrmcghie@users.noreply.github.com>
2022-11-07 21:13:27 -0500

    Update main.tf

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -7,7 +7,7 @@ provider "aws" {
   region = "us-east-1"
   }
   
-  resource "aws_instance" "hi_kura" {
+  resource "aws_instance" "hi_kura"{
   ami = "ami-09d3b3274b6c5d4aa"
   instance_type = "t2.micro"
   key_name = "kuradeploy"
@@ -24,3 +24,4 @@ provider "aws" {
   value = aws_instance.hi_kura.public_ip
   
 }
+
