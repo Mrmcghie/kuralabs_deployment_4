047651ff0d49444c725791fa324888ff7f059923
Mrmcghie <53179879+Mrmcghie@users.noreply.github.com>
2022-11-07 21:27:27 -0500

    Update main.tf

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -4,10 +4,10 @@ variable "aws_secret_key" {}
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region     = "us-east-1"
+  region     = "us-east-1a"
 }
 
- resource "aws_instance" "hi_kura" {
+ resource "aws_instance" "hi20850" {
    ami                    = "ami-09d3b3274b6c5d4aa"
    instance_type          = "t2.micro"
    key_name               = "kuradeploy"
@@ -22,7 +22,7 @@ provider "aws" {
 }
 
  output "instance_ip" {
-   value = aws_instance.hi_kura.public_ip
+   value = aws_instance.hi20850.public_ip
 
 }
 
