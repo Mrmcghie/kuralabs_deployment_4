c30e5d2f593ae697f2fd5cf52e9a0227f3c37dc4
Mrmcghie <53179879+Mrmcghie@users.noreply.github.com>
2022-11-08 01:12:37 -0500

    Update main.tf

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -37,12 +37,11 @@ provider "aws" {
 }
 
 resource "aws_route_table" "prod-public-crt" {
-    vpc_id = "${aws_vpc.main-vpc.id}"
-    
+   vpc_id = "${aws_vpc.main-vpc.id}"
     route {
         //associated subnet can reach everywhere
         cidr_block = "0.0.0.0/0"
-                //CRT uses this IGW to reach internet
+       //CRT uses this IGW to reach internet
         gateway_id = "${aws_internet_gateway.prod-igw.id}" 
     }
     
