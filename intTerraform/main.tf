44b710febac2933fa337c3533dc0ca106d068688
Mrmcghie <53179879+Mrmcghie@users.noreply.github.com>
2022-11-07 21:23:48 -0500

    Update main.tf

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -4,24 +4,25 @@ variable "aws_secret_key" {}
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region = "us-east-1"
-  }
-  
-  resource "aws_instance" "hi_kura"{
-  ami = "ami-09d3b3274b6c5d4aa"
-  instance_type = "t2.micro"
-  key_name = "kuradeploy"
-  vpc_security_group_ids = [aws_security_group.web_ssh.id]
+  region     = "us-east-1"
+}
 
-  user_data = "${file("deploy.sh")}"
+ resource "aws_instance" "hi_kura" {
+   ami                    = "ami-09d3b3274b6c5d4aa"
+   instance_type          = "t2.micro"
+   key_name               = "kuradeploy"
+   vpc_security_group_ids = [aws_security_group.web_ssh.id]
+
+   user_data = "${file("deploy.sh")}"
+
 
   tags = {
     "Name" : "Webserver001"
   }
-  }
-  
-  output "instance_ip" {
-  value = aws_instance.hi_kura.public_ip
-  
+}
+
+ output "instance_ip" {
+   value = aws_instance.hi_kura.public_ip
+
 }
 
