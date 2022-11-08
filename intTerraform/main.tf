809c8b3e15332766cf7b85c75bcc57e81d9978ee
Mrmcghie <53179879+Mrmcghie@users.noreply.github.com>
2022-11-08 01:51:23 -0500

    Update main.tf

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -50,8 +50,8 @@ resource "aws_route_table" "prod-public-crt" {
 }
 
 resource "aws_route_table_association" "prod-crta-public-subnet-1"{
-   subnet_id = "${aws_subnet.prod-subnet-public-1.id}"
-    route_table_id = "${aws_route_table.prod-public-crt.id}"
+   subnet_id      = "${aws_subnet.prod-subnet-public-1.id}"
+   route_table_id = "${aws_route_table.prod-public-crt.id}"
 }
  resource "aws_instance" "Webserver001" { 
    ami                    = "ami-09d3b3274b6c5d4aa"
